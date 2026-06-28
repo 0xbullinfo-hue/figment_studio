d3b7402ab7b5430f2f4bca81e2f2cfc2697a649a
Figment Studio <studio@figment.ng>
2026-06-28 02:01:08 +0100

    chore: clean up temporary batch files

--- push_animation_updates.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git add -A
-git commit -m "feat: use Press Play cover for all gallery animations and add 3D-Villa-Animation-Abuja to Signature section with custom villa cover"
-git push origin master
-echo Push completed with exit code: %ERRORLEVEL%
--- push_ondo_fix.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git add constants.ts components/PortfolioGallery.tsx
-git commit -m "fix: update Ondo metadata to Residential and rename title to Ondo Modern Residence"
-git push origin master
-echo Push completed with exit code: %ERRORLEVEL%
